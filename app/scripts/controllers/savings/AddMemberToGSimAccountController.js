5f580a5cdaa7b4a61d2e778ede116b574c9b999b
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2023-01-25 19:51:42 +0300

    MON-114:- search GSIM Members who don't have account. Display existing Members

--- app/scripts/controllers/savings/AddMemberToGSimAccountController.js
+++ app/scripts/controllers/savings/AddMemberToGSimAccountController.js
@@ -1,17 +1,23 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
         AddMemberToGSimAccountController: function ($q, scope, routeParams, route, location, resourceFactory, $uibModal) {
+                scope.groupId=routeParams.groupId;
+                scope.gsimAccountNumber=routeParams.gsimAccountNumber;
+                scope.parentGSIMId=routeParams.parentGSIMId;
+                scope.allMembers = [];
 
                 scope.clientOptions = function(value){
                 var deferred = $q.defer();
-                resourceFactory.groupGSIMAccountResource.getAllGSimAccountMembersWithOutSavingsAccount({displayName: value, orderBy : 'displayName', parentGSIMId : scope.parentGSIMId,
-                sortOrder : 'ASC', orphansOnly : true}, function (data) {
-                    deferred.resolve(data.pageItems);
+                resourceFactory.groupGSIMAccountResource.getAllGSimAccountMembersWithOutSavingsAccount({groupId: scope.groupId,parentGSIMAccountNo:scope.gsimAccountNumber,name: value, orderBy : 'name', sortOrder : 'ASC'}, function (data) {
+                    console.log(data);
+                    deferred.resolve(data);
                 });
                 return deferred.promise;
                 };
 
-
+                 resourceFactory.groupGSIMAccountResource.get({groupId: scope.groupId,parentGSIMAccountNo:scope.gsimAccountNumber}, function (data) {
+                 scope.allMembers = data[0].childGSIMAccounts;
+                });
         }
     });
     mifosX.ng.application.controller('AddMemberToGSimAccountController', ['$q','$scope', '$routeParams', '$route', '$location', 'ResourceFactory', '$uibModal', mifosX.controllers.AddMemberToGSimAccountController]).run(function ($log) {
