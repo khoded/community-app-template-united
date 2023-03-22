ec245351002abfdb786183129b7b57c99ab625ce
jumba <jumba2010@gmail.com>
2023-03-22 12:04:36 +0200

    CI18-136 - Bug fix when approving a loan

--- app/scripts/controllers/loanAccount/NewLoanAccAppController.js
+++ app/scripts/controllers/loanAccount/NewLoanAccAppController.js
@@ -156,7 +156,7 @@
 
                 scope.multiDisburseLoan = scope.loanaccountinfo.multiDisburseLoan;
                 scope.formData.productId = scope.loanaccountinfo.loanProductId;
-                 scope.formData.loanProductName = scope.loanaccountinfo.loanProductName;
+                scope.loanProductName = scope.loanaccountinfo.loanProductName;
                 scope.formData.fundId = scope.loanaccountinfo.fundId;
                 scope.formData.principal = scope.loanaccountinfo.principal;
                 scope.formData.loanTermFrequency = scope.loanaccountinfo.termFrequency;
