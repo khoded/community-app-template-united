bfc3ee7d404007cb22141d1891196abc3b75b0cc
Faheem Ahmad <faheem@fiter.io>
2023-09-19 00:03:49 +0500

    UC-161 UC-162 UC-163

--- app/scripts/controllers/client/EditFamilyMemberController.js
+++ app/scripts/controllers/client/EditFamilyMemberController.js
@@ -4,7 +4,7 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
         EditFamilyMemberController: function (scope, resourceFactory, routeParams,dateFilter, location) {
-
+            scope.restrictDate = new Date();
             scope.formData={};
             scope.date = {};
            clientId=routeParams.clientId;
@@ -62,6 +62,26 @@
                 this.formData.dateFormat = scope.df;
 
                 if(scope.date.dateOfBirth){
+                    for(var z=0;z<scope.relationshipIdOptions.length;z++) {
+                        if (scope.relationshipIdOptions[z].id == scope.formData.relationshipId) {
+                            var itemSelected = scope.relationshipIdOptions[z];
+                            var selectedValue = itemSelected.name;
+                            if (selectedValue != 'Guardian' && selectedValue != 'Parent') {
+                                break;
+                            } else {
+                                var newDate = new Date();
+                                newDate.setFullYear(new Date().getFullYear() - 18);
+                                var pickerDate = new Date(dateFilter(scope.date.dateOfBirth, scope.df));
+                                if (pickerDate > newDate) {
+                                    scope.date.dateOfBirth.dateOfBirthError = true;
+                                    return;
+                               } else {
+                                    scope.date.dateOfBirth.dateOfBirthError = false;
+
+                               }
+                            }
+                        }
+                    }
                     this.formData.dateOfBirth = dateFilter(scope.date.dateOfBirth,  scope.df);
                 }
                 resourceFactory.familyMember.put({clientId:clientId,clientFamilyMemberId:familyMemberId},scope.formData,function(data)
